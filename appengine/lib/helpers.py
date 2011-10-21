b6908b68dcf63ae5accbfb737d5980e77c9cbdcc
Anthony Bowyer-Lowe <ynohtna@ynohtna.org>
2011-10-21 12:26:18 +0100

    Added simple cleanse_word helper to lib.helpers.

--- appengine/lib/helpers.py
+++ appengine/lib/helpers.py
@@ -1,3 +1,15 @@
+# ------------------------------------------------------------
+def cleanse_word(word):
+    if not word:
+        return
+
+    word = unicode(word, errors = 'ignore')
+
+    import re, unicodedata
+    value = unicodedata.normalize('NFKD', word).encode('ascii', 'ignore')
+    value = unicode(re.sub('[^\w\s!\.,:-=]', '', value).strip().lower())
+    return value
+
 # ============================================================
 def dbg():
     import pdb, sys
