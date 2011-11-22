7b783a9823ff33f931a3dfbe06d08c98fb3980fd
Anthony Bowyer-Lowe <ynohtna@ynohtna.org>
2011-11-22 13:51:47 +0000

    Added initial context free grammar module implementation to repository.

---
+++ appengine/lib/cfg.py
@@ -0,0 +1,59 @@
+class ContextFree(object):
+
+    def __init__(self):
+        self.rules = dict()
+
+    # rules are stored in self.rules, a dictionary; the rules themselves are
+    # lists of expansions (which themselves are lists)
+    def add_rule(self, rule, expansion):
+        if rule in self.rules:
+            self.rules[rule].append(expansion)
+        else:
+            self.rules[rule] = [expansion]
+
+    def expand(self, start):
+        from random import choice
+
+        # if the starting rule was in our set of rules, then we can expand it
+        if start in self.rules:
+            possible_expansions = self.rules[start]
+
+            # grab one possible expansion
+            random_expansion = choice(possible_expansions)
+
+            # call this method again with the current element of the expansion
+            for elem in random_expansion:
+                self.expand(elem)
+        else:
+            # if the rule wasn't found, then it's a terminal: simply append the
+            # string to the expansion
+            self.expansion.append(start)
+
+    # utility method to run the expand method and return the results
+    def get_expansion(self, axiom):
+        self.expansion = list()
+        self.expand(axiom)
+        return self.expansion
+
+
+# ----------------------------------------
+if __name__ == '__main__':
+    cfree = ContextFree()
+    cfree.add_rule('S', ['NP', 'VP'])
+    cfree.add_rule('NP', ['the', 'N'])
+    cfree.add_rule('N', ['cat'])
+    cfree.add_rule('N', ['dog'])
+    cfree.add_rule('N', ['weinermobile'])
+    cfree.add_rule('N', ['duchess'])
+    cfree.add_rule('VP', ['V', 'the', 'N'])
+    cfree.add_rule('V', ['sees'])
+    cfree.add_rule('V', ['chases'])
+    cfree.add_rule('V', ['lusts after'])
+    cfree.add_rule('V', ['blames'])
+
+    expansion = cfree.get_expansion('S')
+    print ' '.join(expansion)
+
+    cfree.add_rule('P', ['S', 'which', 'V', 'S'])
+    expansion = cfree.get_expansion('P')
+    print ' '.join(expansion)
